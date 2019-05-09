99b30e38cd3462b289f45b73cd305858b5fea4b4
AXsmasher <keeswtucker@gmail.com>
2019-05-10 07:57:18 +1200

    Moons

--- Assets/Scripts/PlanetGravity.cs
+++ Assets/Scripts/PlanetGravity.cs
@@ -6,6 +6,7 @@ public class PlanetGravity : MonoBehaviour
 {
     public float mass;
     public float massMultiplier;
+    public List<Rigidbody2D> players = new List<Rigidbody2D>();
     public Rigidbody2D player;
     public float forceComponentConstant;
     private float UniversalGravityConstant;
@@ -15,6 +16,8 @@ public class PlanetGravity : MonoBehaviour
     public Vector2 position;
     public int gravMultiplier;
     public CircleCollider2D colliderCircle;
+    public Rigidbody2D rb;
+    public bool affectPlanets = true;
 
     public int BlackWhiteHoleMultiplier = 20;
 
@@ -29,7 +32,14 @@ public class PlanetGravity : MonoBehaviour
 
         mass = 0.75f * Mathf.PI * Mathf.Pow(transform.localScale.x, 3f) * massMultiplier;
 
-        player = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+
+        foreach (GameObject GO in GameObject.FindGameObjectsWithTag("dynamic"))
+        {
+            players.Add(GO.GetComponent<Rigidbody2D>());
+        }
 
         position = transform.position;
 
@@ -56,7 +66,7 @@ public class PlanetGravity : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.tag == "Player")
+        if (col.tag == "Player" && col.GetComponent<ControlPlayer>())
         {
             if (FindObjectOfType<Camera>().backgroundColor == Color.black)
             {
@@ -103,6 +113,24 @@ public class PlanetGravity : MonoBehaviour
         if (active)
         {
             player.AddForce(force);
+        }
+
+        if (affectPlanets)
+        {
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (players[i] != rb)
+                {
+                    direction = AngleBetweenPoints(transform.position, players[i].transform.position);
+                    distance = Vector2.Distance(transform.position, players[i].transform.position);
+                    force = (forceComponentConstant * direction * players[i].mass) / (distance * distance);
+
+                    if (active)
+                    {
+                        players[i].AddForce(force);
+                    }
+                }
+            }
         }
     }
 
