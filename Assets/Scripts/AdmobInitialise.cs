0d56bcc93acd49519af5fcd910459e872b13bc2a
AXsmasher <keeswtucker@gmail.com>
2019-05-14 09:59:08 +1200

    Fixed Crash

--- Assets/Scripts/AdmobInitialise.cs
+++ Assets/Scripts/AdmobInitialise.cs
@@ -5,7 +5,11 @@ public class AdmobInitialise : MonoBehaviour
 {
     void Start()
     {
-        string appId = "ca-app-pub-3563227024265510~4121574348";
+        #if UNITY_ANDROID
+            string appId = "ca-app-pub-3563227024265510~4121574348";
+        #else
+            string appId = "unexpected_platform";
+        #endif
 
         // Initialize the Google Mobile Ads SDK.
         MobileAds.Initialize(appId);
